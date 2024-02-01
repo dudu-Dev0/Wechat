41f3100609aad0ccb574a0773fb59a37da95ac35
dudu <duyuao200849@163.com>
2024-02-01 14:36:47 +0800

    first push

---
+++ app/build.gradle.kts
@@ -0,0 +1,52 @@
+
+plugins {
+    id("com.android.application")
+    
+}
+
+android {
+    namespace = "com.dudu.wechat"
+    compileSdk = 33
+    
+    defaultConfig {
+        applicationId = "com.dudu.wechat"
+        minSdk = 25
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+        
+        vectorDrawables { 
+            useSupportLibrary = true
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+        
+    }
+    
+}
+
+dependencies {
+
+
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    // Standard Wear OS libraries
+    implementation("androidx.wear:wear:1.2.0")
+    // includes support for wearable specific inputs
+    implementation("androidx.wear:wear-input:1.1.0")
+}
