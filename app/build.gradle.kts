9413b932dab59ecad7ad4b86a964ec98f89c4a22
dudu <duyuao200849@163.com>
2024-02-04 19:13:04 +0800

    updated wear library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,7 +44,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     // Standard Wear OS libraries
-    implementation("androidx.wear:wear:1.1.0")
+    implementation("androidx.wear:wear:1.3.0")
     //retrofit
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
